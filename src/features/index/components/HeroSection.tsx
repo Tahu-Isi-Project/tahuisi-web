0ff97b76bc90856e4a88aad8b34d276306b5127d
Akarui26 <68172308+Akarui26@users.noreply.github.com>
2025-12-28 19:09:15 +0700

    First Commit

---
+++ src/features/index/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import "@features/index/styles/theme.css";
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className="hero-section"></section>
+    </>
+  );
+};
+
+export default HeroSection;
