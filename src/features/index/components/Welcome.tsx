3269df1b4848dbfda8e41c0242c06086b3d56713
alfahadiraihan <raihanhadi100@gmail.com>
2025-12-29 22:10:34 +0700

    feat: basic navbar

--- src/features/index/components/Welcome.tsx
+++ src/features/index/components/Welcome.tsx
@@ -1,14 +1,10 @@
 const Welcome = () => {
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <main>
-          <section className="text-8xl">
-            <h1>Ini website Tahu Isi</h1>
-          </section>
-        </main>
-      </div>
-    </>
+    <div className="flex h-screen items-center justify-center bg-black">
+      <section className="text-8xl text-amber-50">
+        <h1>Ini website Tahu Isi</h1>
+      </section>
+    </div>
   );
 };
 
