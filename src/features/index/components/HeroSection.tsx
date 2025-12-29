1de071c21789b556a0df5459fca7e5c92093fbd5
alfahadiraihan <raihanhadi100@gmail.com>
2025-12-29 22:32:06 +0700

    feat: temporary hero section

--- src/features/index/components/HeroSection.tsx
+++ src/features/index/components/HeroSection.tsx
@@ -1,5 +1,11 @@
-const HeroSection = () => {
-  return <section className="hero-section" />;
+const Welcome = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+      <section className="hero-section text-8xl text-amber-50">
+        <h1>Ini website Tahu Isi</h1>
+      </section>
+    </div>
+  );
 };
 
-export default HeroSection;
+export default Welcome;
