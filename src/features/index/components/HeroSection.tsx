d2acda9acc5e91d2a00375a97e852c9a2d349934
Akarui26 <68172308+Akarui26@users.noreply.github.com>
2026-01-01 18:10:56 +0700

    fix aside and responsive

--- src/features/index/components/HeroSection.tsx
+++ src/features/index/components/HeroSection.tsx
@@ -21,8 +21,8 @@ const HeroSection = () => {
           src={communityLogo.src}
           alt="Community Logo"
         />
-        <div className="hero__text-wrap absolute  w-full  h-screen z-10">
-          <aside className="top-1/2 absolute">
+        <div className="hero__text-wrap absolute  w-full  h-full z-10">
+          <aside className="top-1/2 absolute transform-[translateY(-50%)]">
             <ul className="flex flex-col gap-6">
               {asideMenuList.map((item, idx) => (
                 <li className="hero__aside-menu py-5 font-[Allerta] text-[40px] text-right min-w-[393px] bg-white after:bg-[#EE6983] after:w-[16px] relative inline-flex items-center px-6 font-semibold text-black after:content-[''] after:absolute after:top-0 after:right-0 after:h-full">
@@ -34,7 +34,7 @@ const HeroSection = () => {
             </ul>
           </aside>
 
-          <h1 className="text-[140px] ml-[4%] leading-none bottom-0 max-w-[1244px]  absolute font-[LuckiestGuy] text-white text-shadow-[0_0_50px_rgba(0,0,0,0.8)] inline-block">
+          <h1 className="text-[140px] pb-[4%] ml-[4%] leading-none bottom-0 max-w-[1244px]  absolute font-[LuckiestGuy] text-white text-shadow-[0_0_50px_rgba(0,0,0,0.8)] inline-block">
             CIPTAKAN DUNIAMU DI GENSOKYO
           </h1>
         </div>
