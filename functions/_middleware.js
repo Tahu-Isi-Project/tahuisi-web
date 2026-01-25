e503c1f533468fd0627933eada02c0cc98df1646
alfahadiraihan <raihanhadi100@gmail.com>
2026-01-25 21:17:11 +0700

    this better works for the headers

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,8 +2,10 @@ export async function onRequest(context) {
   const response = await context.next();
   const url = new URL(context.request.url);
 
-  if (url.pathname === "/")
+  if (url.pathname === "/") {
     response.headers.set("Cache-Control", "public, max-age=60, s-maxage=86400");
+    response.headers.set("X-Homepage-Test", "Test header");
+  }
 
   if (url.pathname === "/article")
     response.headers.set("Cache-Control", "public, max-age=30, s-maxage=86400");
