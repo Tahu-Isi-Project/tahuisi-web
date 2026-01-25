07e742597dddf62cf7f760faaf8995456d48928e
alfahadiraihan <raihanhadi100@gmail.com>
2026-01-25 20:06:47 +0700

    why tf is the caching not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
 
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
       envFiles: ["./.env"]
     },
-    imageService: "compile"
+    imageService: "cloudflare"
   }),
 
   integrations: [react()],
