16e73851da09f86ed34d7c706bf53155c018002e
Ryhun <raihanhadi100@gmail.com>
2026-04-22 22:09:16 +0700

    build: replace cloudflare adapter with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-      envFiles: ["./.env"]
-    },
-    imageService: "cloudflare"
-  }),
-
+  output: "server",
   integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 
   redirects: {
     "/event": {
@@ -39,5 +28,13 @@ export default defineConfig({
       status: 308,
       destination: "/",
     }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
